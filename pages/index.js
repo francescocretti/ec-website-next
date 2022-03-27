850eeebf7bc9197881235db10a0ee1d3c009cc63
francescocretti <francesco.cretti@gmail.com>
2022-03-27 19:09:56 +0200

    add link to ec logo

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,12 @@ const Home = () => {
       <Container bgImage={image} >
         <div className='flex flex-col justify-center items-center w-full h-full'>
 
-
-
-          <EcLogo
-            className="cursor-pointer"
-            width={LOGO_SIZE} height={LOGO_SIZE} color="#26161E"
-          />
+          <a href="https://youtu.be/k7wUWpOObU0"          >
+            <EcLogo
+              className="cursor-pointer"
+              width={LOGO_SIZE} height={LOGO_SIZE} color="#26161E"
+            />
+          </a>
 
           <h1 className='font-inknut font-bold text-3xl md:text-3xl text-ec-grayred-500 text-center mt-10'>
             Psychedelic grooves from the world

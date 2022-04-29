e2fb8e2c266cd3cd9609b85ca99699e89e7157ca
francescocretti <francesco.cretti@gmail.com>
2022-04-29 18:16:36 +0200

    add Tom to homepage

---
+++ components/NewVideo.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+import AnimatedArrow from './AnimatedArrow';
+
+import Link from 'next/link';
+
+const NewVideo = ({ videoUrl, image }) => {
+  return (
+    <div className='relative flex flex-col justify-center text-center'>
+
+      <div className='hidden md:block absolute -left-[240px] top-[160px]'>
+        <AnimatedArrow />
+      </div>
+
+      <div className='block md:hidden absolute -left-[80px] top-[0px] rotate-45'>
+        <AnimatedArrow width={100} height={50} />
+      </div>
+
+      <div className='absolute -left-[260px] top-[190px]'>
+        <span className='inline-block font-bold text-black bg-gray-300 text-xl -rotate-[23deg]'>NEW VIDEO!</span>
+      </div>
+
+      <div className='relative w-[150px] h-[250px] cursor-pointer transition-transform duration-700 hover:scale-110'>
+        <Link href={videoUrl}>
+          <a>
+            <Image
+              src={image.src}
+              blurDataURL={image.blurDataURL}
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+              placeholder="blur"
+              priority
+            />
+          </a>
+        </Link>
+
+      </div>
+      <p className='font-bold mt-4 text-xl'>CLICK THIS GUY!</p>
+    </div>
+
+  )
+}
+
+export default NewVideo
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-import { useState } from 'react';
-
 import EcLogo from '../components/EcLogo';
-
-import { useMountEffect } from '../hooks/toolkit';
 
 import Container from '../components/Container';
 
@@ -10,21 +6,28 @@ import image from '../assets/home-min.png';
 
 import SocialLinks from '../components/SocialLinks';
 
+import Link from 'next/link';
+
+import tom from '../assets/tom.png';
+
+import NewVideo from '../components/NewVideo';
+
 const LOGO_SIZE = 150;
 
 const Home = () => {
-
   return (
     <>
       <Container bgImage={image} >
-        <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='my-28 md:my-0 flex flex-col justify-center items-center w-full h-full'>
 
-          <a href="https://youtu.be/k7wUWpOObU0">
-            <EcLogo
-              className="cursor-pointer"
-              width={LOGO_SIZE} height={LOGO_SIZE} color="#26161E"
-            />
-          </a>
+          <Link href="https://youtu.be/k7wUWpOObU0">
+            <a>
+              <EcLogo
+                className="cursor-pointer"
+                width={LOGO_SIZE} height={LOGO_SIZE} color="#26161E"
+              />
+            </a>
+          </Link>
 
           <h1 className='font-inknut font-bold text-3xl md:text-3xl text-ec-grayred-500 text-center mt-10'>
             Psychedelic grooves from the world
@@ -32,6 +35,10 @@ const Home = () => {
 
           <div className="mt-10">
             <SocialLinks />
+          </div>
+
+          <div className="mt-24 md:mt-16">
+            <NewVideo videoUrl="https://youtu.be/k7wUWpOObU0" image={tom} />
           </div>
 
         </div>
